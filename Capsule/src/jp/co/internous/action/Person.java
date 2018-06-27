a035faf24f99bbe8ae9fd52e2b346c43d77dd38f
shinnnosuke1109 <hal101_flower@yahoo.co.jp>
2018-06-27 17:12:28 +0900

    6/27

--- Capsule/src/jp/co/internous/action/Person.java
+++ Capsule/src/jp/co/internous/action/Person.java
@@ -4,7 +4,11 @@ public class Person {
 	private String name= null;
 	private int age= 0;
 
-	public Person(String name, int age){}
+	public Person(String name, int age){
+
+		this.name= name;
+		this.age= age;
+	}
 
 
 	public String getName(){
